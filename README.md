49bd4616877c12c613f6c07c86d67e9468213cdb
kaktuxmx <kaktuxsql@gmail.com>
2018-02-16 23:22:01 -0700

    Push the file structure and server.js file update

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Welcome to the FriendFinder APP!
+
+** This app will help you connect with your friends base on a 
---
+++ server.js
@@ -0,0 +1,22 @@
+// Require packages from NPM to use in the APP
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Advising NODE we are seting up a Express Server
+const app = express()
+
+// Sets port for the Server.
+const PORT = process.env.PORT || 8080;
+
+// sets the body parser
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Listener for the Server
+
+app.listen(PORT, function() {
+    console.log("App listening on port " + PORT);
+});
